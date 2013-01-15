647e9b6252c4c367186e7fd5b1a28f2b6eb0f5b1
Michel Albert <michel@albert.lu>
2013-01-15 11:16:25 +0100

    Additional functionality.
    * `makeDocument` factory method
    * attribute assignment supported
    * default constructor added (with `self` URL)
    * access by key added (`print doc['key']`)
    * iterator over keys added.

--- hal.py
+++ hal.py
@@ -11,22 +11,75 @@ LOG = logging.getLogger(__name__)
 
 class Document(object):
 
-    def __init__(self, document_source):
-        if isinstance(document_source, dict):
-            self.data = document_source
-        elif hasattr(document_source, 'read'):
-            self.data = json.load(document_source)
+    @staticmethod
+    def makeDocument(obj):
+        """
+        Constructs a HAL document from an object.
+        """
+        instance = Document()
+        if isinstance(obj, dict):
+            instance._data = obj
+        elif hasattr(obj, 'read'):
+            instance._data = json.load(obj)
+        elif isinstance(obj, basestring):
+            instance._data = json.loads(obj)
+        elif isinstance(obj, list):
+            instance = [Document.makeDocument(_) for _ in obj]
         else:
-            self.data = json.loads(document_source)
+            raise NotImplementedError('Unable to instantiate a HAL Document '
+                    'from %r' % obj)
+        return instance
+
+    def __init__(self, selfurl=u''):
+        self._data = {'_links': {'self': {'href': selfurl}}}
 
     def __getattribute__(self, attr):
         LOG.debug('Retrieving %r from %r' % (attr, self))
-        data = object.__getattribute__(self, 'data')
+        try:
+            return object.__getattribute__(self, attr)
+        except AttributeError:
+            pass
+
+        data = object.__getattribute__(self, '_data')
         if attr in data:
             return data[attr]
         elif "_embedded" in data and attr in data['_embedded']:
-            return Document(data['_embedded'][attr])
+            return Document.makeDocument(data['_embedded'][attr])
         elif "_links" in data and attr in data['_links']:
             raise NotImplementedError('Dynamically retrieving attributes '
                     'from "_links" is not yet supported!')
         raise AttributeError('%r has no attribute %r' % (self, attr))
+
+    def __repr__(self):
+        data = object.__getattribute__(self, '_data')
+        return "<Document %s>" % (data['_links']['self']['href'])
+
+    def __iter__(self):
+        data = object.__getattribute__(self, '_data')
+        for key in data:
+            if key == '_embedded':
+                for embedded_key in data[key]:
+                    yield embedded_key
+            elif key == '_links':
+                # we'll ignore links when iterating over the keys. For now.
+                pass
+            else:
+                yield key
+
+    def __getitem__(self, key):
+        return getattr(self, key)
+
+    def __setattr__(self, name, value):
+        if name == '_data':
+            object.__setattr__(self, name, value)
+        else:
+            data = object.__getattribute__(self, '_data')
+            if isinstance(value, Document) or (
+                    isinstance(value, list) and all([isinstance(_, Document) for _ in value])):
+                data['_embedded'][name] = value.asdict()
+            else:
+                data[name] = value
+
+    def asdict(self):
+        data = object.__getattribute__(self, '_data')
+        return data
--- test.py
+++ test.py
@@ -3,7 +3,19 @@ logging.basicConfig(level=logging.DEBUG)
 
 from hal import Document
 
-doc = Document(open('example.json'))
+doc = Document.makeDocument(open('example.json'))
 print doc.title
 #print doc.author
-print doc.tags.items
+print doc.tags
+print 80 * "-"
+for key in doc:
+    print key, doc[key]
+
+print 80 * "-"
+doc.foo = 10
+print doc.foo
+
+tmpdoc = Document('/foo/bar')
+tmpdoc.bar = 'baz'
+doc.bar = tmpdoc
+print doc.bar.asdict()
